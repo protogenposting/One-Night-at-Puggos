623127414e8beaf0d6b8e807dab4cd2270ecab75
protogenposting <86360309+protogenposting@users.noreply.github.com>
2025-10-11 12:25:30 -0400

    bleh

--- Rooms/gameplay.gd
+++ Rooms/gameplay.gd
@@ -12,6 +12,17 @@ func _ready() -> void:
 	currentCamera = cameras[camID]
 	
 	currentCamera.get_camera_projection()
+	
+	var all20 = true
+	
+	for i in EnemyAI.enemyAiValues:
+		if i < 20:
+			all20 = false
+	
+	if !all20:
+		$NormalTheme.play()
+	else:
+		$All20Theme.play()
 
 func _process(delta: float) -> void:
 	currentCamera = cameras[camID]
